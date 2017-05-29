5004da37cee576024e17e4b244e4577334bff1e2
itggot-anna-johannesson <anna.johannesson@itggot.se>
2017-05-29 11:02:03 +0200

    Home
    Homeknappen fungerar för att ta bort info

--- site/js/main.js
+++ site/js/main.js
@@ -24,3 +24,22 @@ function info3(){
   var element3 = document.querySelector('.information3');
   element3.classList.remove('hide')
 }
+
+function home(){
+  var element = document.querySelector('.article_one');
+  var element2 = document.querySelector('.article_two');
+  var element3 = document.querySelector('.article_three');
+
+  element.classList.remove('hide')
+  element2.classList.remove('hide')
+  element3.classList.remove('hide')
+
+  var element4 = document.querySelector('.information');
+  var element5 = document.querySelector('.information2');
+  var element6 = document.querySelector('.information3');
+
+  element4.classList.add('hide')
+  element5.classList.add('hide')
+  element6.classList.add('hide')
+
+}

d01b802c58e3e1136ece477f6d59b6178c60131c
itggot-anna-johannesson <anna.johannesson@itggot.se>
2017-05-29 09:55:41 +0200

    Info
    Javascript och Html för att öppna info om artiklarna

---
+++ site/js/main.js
@@ -0,0 +1,26 @@
+function info1(){
+  var element = document.querySelector('.article_two');
+  element.classList.add('hide')
+  var element2 = document.querySelector('.article_three');
+  element2.classList.add('hide')
+  var element3 = document.querySelector('.information');
+  element3.classList.remove('hide')
+}
+
+function info2(){
+  var element = document.querySelector('.article_one');
+  element.classList.add('hide')
+  var element2 = document.querySelector('.article_three');
+  element2.classList.add('hide')
+  var element3 = document.querySelector('.information2');
+  element3.classList.remove('hide')
+}
+
+function info3(){
+  var element = document.querySelector('.article_one');
+  element.classList.add('hide')
+  var element2 = document.querySelector('.article_two');
+  element2.classList.add('hide')
+  var element3 = document.querySelector('.information3');
+  element3.classList.remove('hide')
+}
